a454a712434a5796e510085efa741ad4200200d7
ferdi.kurnaz@megaendustri.com.tr <ferdi.kurnaz@megaendustri.com.tr>
2026-02-27 13:38:40 +0300

    feat: edited changelog, readme, .csproj

--- src/Resultify.Core/Models/Result.Generic.cs
+++ src/Resultify.Core/Models/Result.Generic.cs
@@ -8,11 +8,77 @@ public sealed class Result<T> : BaseResult
 {
     public T? Value { get; }
 
-    private Result(bool isSuccess, Error error, T? value) : base(isSuccess, error) 
+    private Result(bool isSuccess, Error error, T? value) : base(isSuccess, error)
         => Value = value;
-    
+
 
     public static Result<T> Success(T value) => new(true, Error.None, value);
     public static Result<T> Failure(Error error) => new(false, error, default);
+
+
+
+    public void Match(Action<T?> onSuccess, Action<Error> onFailure)
+    {
+        if (onSuccess is null) throw new ArgumentNullException(nameof(onSuccess));
+        if (onFailure is null) throw new ArgumentNullException(nameof(onFailure));
+
+        if (IsSuccess)
+            onSuccess(Value);
+        else
+            onFailure(Error);
+    }
+
+    public TResult Match<TResult>(Func<T?, TResult> onSuccess, Func<Error, TResult> onFailure)
+    {
+        if (onSuccess is null) throw new ArgumentNullException(nameof(onSuccess));
+        if (onFailure is null) throw new ArgumentNullException(nameof(onFailure));
+
+        return IsSuccess
+            ? onSuccess(Value)
+            : onFailure(Error);
+    }
+
+    public Result<TResult> Map<TResult>(Func<T?, TResult> func)
+    {
+        if (func is null) throw new ArgumentNullException(nameof(func));
+
+        return IsSuccess
+            ? Result<TResult>.Success(func(Value))
+            : Result<TResult>.Failure(Error);
+    }
+
+    public Result<T> Map(Action<T?> action)
+    {
+        if (action is null) throw new ArgumentNullException(nameof(action));
+
+        if (IsSuccess)
+            action(Value);
+
+        return this;
+    }
+
+    public Result<TResult> Bind<TResult>(Func<T?, Result<TResult>> func)
+    {
+        if (func is null) throw new ArgumentNullException(nameof(func));
+
+        return IsSuccess
+            ? func(Value)
+            : Result<TResult>.Failure(Error);
+    }
+
+    public static Result<T> Try(Func<T> func)
+    {
+        if (func is null) throw new ArgumentNullException(nameof(func));
+
+        try
+        {
+            var value = func();
+            return Success(value);
+        }
+        catch (Exception ex)
+        {
+            return Failure(new Error(Code: ex.GetType().Name, Description: ex.Message));
+        }
+    }
 
 }
--- src/Resultify.Core/Models/Result.cs
+++ src/Resultify.Core/Models/Result.cs
@@ -13,4 +13,67 @@ public sealed class Result : BaseResult
     public static Result Success() => new(true, Error.None);
     public static Result Failure(Error error) => new(false, error);
 
+
+    public void Match(Action onSuccess, Action<Error> onFailure)
+    {
+        if (onSuccess is null) throw new ArgumentNullException(nameof(onSuccess));
+        if (onFailure is null) throw new ArgumentNullException(nameof(onFailure));
+
+        if (IsSuccess)
+            onSuccess();
+        else
+            onFailure(Error);
+    }
+
+    public TResult Match<TResult>(Func<TResult> onSuccess, Func<Error, TResult> onFailure)
+    {
+        if (onSuccess is null) throw new ArgumentNullException(nameof(onSuccess));
+        if (onFailure is null) throw new ArgumentNullException(nameof(onFailure));
+
+        return IsSuccess
+            ? onSuccess()
+            : onFailure(Error);
+    }
+
+    public Result<T> Map<T>(Func<T> func)
+    {
+        if (func is null) throw new ArgumentNullException(nameof(func));
+
+        return IsSuccess
+            ? Result<T>.Success(func())
+            : Result<T>.Failure(Error);
+    }
+    public Result Map(Func<Result> func)
+    {
+        if (func is null) throw new ArgumentNullException(nameof(func));
+
+        return IsSuccess
+            ? func()
+            : Failure(Error);
+    }
+
+    public Result Bind(Func<Result> func)
+    {
+        if (func is null) throw new ArgumentNullException(nameof(func));
+
+        return IsSuccess
+            ? func()
+            : Result.Failure(Error);
+    }
+
+    public static Result Try(Action action)
+    {
+        if (action is null) throw new ArgumentNullException(nameof(action));
+
+        try
+        {
+            action();
+            return Success();
+        }
+        catch (Exception ex)
+        {
+            return Failure(new Error(Code: ex.GetType().Name, Description: ex.Message));
+        }
+    }
+
 }
